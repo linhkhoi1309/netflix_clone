7300f9d4cd3217f7ef6f2be561ba98dc97d3d3a3
Long Nguyen <zcmgyu@gmail.com>
2020-08-12 16:43:00 +0900

    Fix bug in Row component

--- src/components/Row.js
+++ src/components/Row.js
@@ -5,11 +5,12 @@ import Youtube from "react-youtube";
 import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
+const DEFAULT_PATH = "https://kestrelwines.com/wp-content/uploads/2019/09/1.jpg"
 
 function Row({ title, url, isLargeRow }) {
   const [movies, setMovies] = useState([]);
   const [trailerUrl, setTrailerUrl] = useState("");
-  const [path, setPath] = useState("");
+  // const [path, setPath] = useState("");
 
   useEffect(() => {
     console.log(url)
@@ -46,24 +47,20 @@ function Row({ title, url, isLargeRow }) {
       <h2>{title}</h2>
       <div className="row-posters">
         {movies.map((movie) => {
-          if (movie.backdrop_path === null) {
-            setPath(
-              "https://kestrelwines.com/wp-content/uploads/2019/09/1.jpg"
-            );
-          } else {
-            setPath(
-              `${base_url}${
-                isLargeRow ? movie.poster_path : movie.backdrop_path
-              }`
-            );
+          const { id, name, backdrop_path, poster_path } = movie;
+          let path = DEFAULT_PATH;
+          if (backdrop_path) {
+            path =
+              `${base_url}${isLargeRow ? poster_path : backdrop_path}`
           }
+
           return (
             <img
+              key={id}
               src={path}
-              alt={movie.name}
-              key={movie.id}
+              alt={name}
               className={`row-poster ${isLargeRow && "row-poster-lg"}`}
-              onClick={_onClick(movie)}
+              onClick={() => _onClick(movie)}
             />
           );
         })}
